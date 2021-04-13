10bb34e2301b73cd3b48a78664c174518c6494af
Sanket Manmode <manmodesanket@gmail.com>
2021-04-13 15:43:11 +0530

    toast added

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 const toggleNav = () => {
     const nav = document.querySelector('.component-list');
     nav.classList.toggle('show');
@@ -9,4 +6,23 @@ const toggleNav = () => {
 
 const toggler = document.querySelector('.toggler');
 
-toggler.addEventListener('click', toggleNav)
+toggler.addEventListener('click', toggleNav);
+
+const toasts = document.querySelectorAll('.toasts');
+const successToast = document.querySelectorAll('.toast-btn');
+
+const handleSucessToast = (toastMessage) => {
+
+    const toastNotification = document.createElement('div');
+    toastNotification.classList.add('toast-success');
+    toastNotification.innerText = `${toastMessage}`
+    toasts[0].appendChild(toastNotification);
+
+    setTimeout(() => {
+        toastNotification.remove();
+    }, 2000);
+
+}
+
+
+successToast[0].addEventListener('click', () => handleSucessToast('This is success toast.'))
